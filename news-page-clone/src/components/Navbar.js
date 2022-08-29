7d12d426009bf35fcdacef23b375058b48e5f130
samueleffa <seffa1@gmail.com>
2022-08-29 06:22:42 -0400

    navbar layout done

--- news-page-clone/src/components/Navbar.js
+++ news-page-clone/src/components/Navbar.js
@@ -3,10 +3,19 @@ import React from "react";
 function Navbar() {
   return (
     <nav className="Navbar">
-      <i class="fa-solid fa-magnifying-glass"></i>
+      <i class="fa-solid fa-magnifying-glass Navbar__search"></i>
       <ul className="Navbar__ul">
-        <li className="Navbar__ul__li"></li>
+        <li className="Navbar__ul__li">Investigations</li>
+        <li className="Navbar__ul__li">Geopolitics & Finance</li>
+        <li className="Navbar__ul__li">Health</li>
+        <li className="Navbar__ul__li">
+          <i class="fa-solid fa-newspaper Navbar__logo"></i>
+        </li>
+        <li className="Navbar__ul__li">Technology</li>
+        <li className="Navbar__ul__li">National Security</li>
+        <li className="Navbar__ul__li">OP/Analysis</li>
       </ul>
+      <i class="fa-solid fa-bars Navbar__lines"></i>
     </nav>
   );
 }
