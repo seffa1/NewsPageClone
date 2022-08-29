81fb75a85d0e48a063ae75c9ebcc85c612029276
samueleffa <seffa1@gmail.com>
2022-08-29 05:49:26 -0400

    init commit

---
+++ news-page-clone/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="Navbar">
+      <i class="fa-solid fa-magnifying-glass"></i>
+      <ul className="Navbar__ul">
+        <li className="Navbar__ul__li"></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
