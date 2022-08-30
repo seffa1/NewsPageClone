4c80b34b439159f7ec92e5abb9c0cfd9bc279248
seffa_work <sameffa1@gmail.com>
2022-08-30 14:19:54 -0400

    got API functionality working

--- news-page-clone/src/App.js
+++ news-page-clone/src/App.js
@@ -3,15 +3,46 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Popular from "./components/Popular";
 import Topic from "./components/Topic";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 // TODO: Populate data with API:
 // https://jasonwatmore.com/post/2020/07/17/react-axios-http-get-request-examples
 
+function newsApiParser(article) {
+  // date formatting
+  let date = new Date(article.publishedAt).toLocaleDateString("en-us", {
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+  });
+
+  return { ...article, date: date };
+}
+
 function App() {
+  const API_KEY = "";
+  const [articles, setArticles] = useState([]);
+
+  // Get top 4 stories
+  useEffect(() => {
+    // GET request using axios inside useEffect React hook
+    axios
+      .get(
+        `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${API_KEY}&pageSize=4`
+      )
+      .then((response) =>
+        setArticles(
+          response.data.articles.map((article) => newsApiParser(article))
+        )
+      )
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
-      <Hero />
+      <Hero article={articles[0]} />
       <div className="container">
         <Popular />
         <Topic title={"Investigations"} link={"1"} />
--- news-page-clone/src/components/Hero.js
+++ news-page-clone/src/components/Hero.js
@@ -1,29 +1,31 @@
 import React from "react";
 import earth from "../images/earth.jpg";
 
-function Hero() {
+function Hero(props) {
   return (
     <section className="Hero">
       <img src={earth} className="Hero__img" />
 
-      <article className="Hero__article">
-        <header className="Hero__article__heading">
-          <div className="Topic-tag Topic-tag--red">TECHNOLOGY</div>
-          <div className="Hero__article__heading__author">Sam Cooper</div>
-          <div className="Hero__article__heading__dot">
-            <i class="fa-solid fa-circle"></i>
-          </div>
-          <div className="Hero__article__heading__date">September 23, 2021</div>
-        </header>
-        <h2 className="Hero__article__title">
-          Canada's cyber agency dismantling fake government coronavirus pandemic
-          response websites
-        </h2>
-        <footer className="Hero__article__footer">
-          Canadian intelligence agencies are fighting against attempts to
-          exploit the coronavirus pandemic.
-        </footer>
-      </article>
+      {props.article && (
+        <article className="Hero__article">
+          <header className="Hero__article__heading">
+            <div className="Topic-tag Topic-tag--red">TECHNOLOGY</div>
+            <div className="Hero__article__heading__author">
+              {props.article.author}
+            </div>
+            <div className="Hero__article__heading__dot">
+              <i class="fa-solid fa-circle"></i>
+            </div>
+            <div className="Hero__article__heading__date">
+              {props.article.date}
+            </div>
+          </header>
+          <h2 className="Hero__article__title">{props.article.title}</h2>
+          <footer className="Hero__article__footer">
+            {props.article.description}
+          </footer>
+        </article>
+      )}
     </section>
   );
 }
