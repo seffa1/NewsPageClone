c297fe4ee8486e8f81d7b9e93b6c11fc60d60a73
samueleffa <seffa1@gmail.com>
2022-08-31 07:00:31 -0400

    added last two components

---
+++ news-page-clone/src/components/Subscribe.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Subscribe() {
+  return (
+    <section className="Subscribe">
+      <h2>Subscribe to The Bureau</h2>
+      <h3>
+        Engaging copy for member subscription with the CTA to enter email and
+        subscribe
+      </h3>
+    </section>
+  );
+}
+
+export default Subscribe;
