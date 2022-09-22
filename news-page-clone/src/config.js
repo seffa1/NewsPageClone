f5a4be6a6e7425b3efaf165b2261bfd80dc37a6e
Sam <94399429+seffa1@users.noreply.github.com>
2022-09-22 16:04:08 -0700

    Update config.js

--- news-page-clone/src/config.js
+++ news-page-clone/src/config.js
@@ -1,3 +1,10 @@
+// If the build is "local" it uses the newsAPI
+// If the build is "production" it uses old saved data
+
+// export default {
+//   build: "production",
+// };
+
 export default {
-  build: "production",
+  build: "local",
 };
